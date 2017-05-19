73575c7a79d2483bacbd84c01ca146368b939232
Andy Dayton <amdayton@gmail.com>
2017-05-18 20:56:32 -0400

    Converted into nyg template

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-import styles from './styles.scss';
+const nyg = require('nyg');
 
-document.write('<h1>Hello World</h1>');
+const prompts = [
+  {
+    type: "input",
+    name: "project",
+    message: "What is the project name?",
+    default: "sketch-"+Date.now()
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Project description?",
+    default: ""
+  },
+  {
+    type: "confirm",
+    message: "run yarn install?",
+    name: "yarn",
+    default: true
+  }
+];
+
+const globs = [
+  {base: 'template/', glob: '*'}
+];
+
+nyg(prompts, globs).run();
